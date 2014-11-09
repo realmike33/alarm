6d8cf932054f7e64e37d3d080847f425958fc108
Mike Moss <realmike33@gmail.com>
2014-11-09 11:09:52 -0800

    Finished alarm. Working on styling

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
 $(function() {
+  var alarms = [];
+  var audio = new Audio('alarm1.mp3');
   var myFunc = function(){
-    var time = moment().format('h:mm:ss a');
-    $('.clock-container').text(time).addClass('clock');
+    var time = moment();
+    $('.clock-container').text(time.format('h:mm:ss a')).addClass('clock');
     var currentTime = function(location, className){
-      $(location).text(time).addClass(className);
+      $(location).text(time.format('h:mm:ss a')).addClass(className);
     };
-
-    currentTime('.clock-container', 'clock');  
+    currentTime('.clock-container', 'clock'); 
+    alarms.forEach(function(val){
+      var diff = time.diff(val, 'seconds');
+      if(diff === 1){
+        audio.play();
+      }
+    }) 
   };
-
-
-
-  setInterval(function(){
+  window.check = setInterval(function(){
     myFunc();
-  }, 1);
+  }, 1000);
 
-  var check = function(){
-    $('button').timepicker();
-    // var time = prompt('Set your Alarm', 'Please follow format: Hour:Minute');
-    // var day = prompt('Is this am or pm?')
-    // return time + day  
-  };
-
-  $('button').on('click', check)  
-
-
-
+  $('button').on('click', function(){
+    var userHours = $('input.time')[0].valueAsNumber;
+    var userMinutes = $('input.time')[1].valueAsNumber;
+    var userAM = $('input.time')[2].checked;
+    var userPM = $('input.time')[3].checked;
+    var dayTime = !!userAM ? "AM" : "PM";
+    var alarmTime = userHours + ':' + userMinutes + dayTime;
+    var alarmMoment = moment({hour: userHours, minute: userMinutes})
+    alarms.push(alarmMoment);
+    console.log(alarmTime, 'UserTime')
+    $('.user-Alarm').text(alarmTime);
+    }) 
 });  
