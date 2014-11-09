ce4857dc0cfb2003b0e10ab95ef025ecfafdf6bd
Mike Moss <realmike33@gmail.com>
2014-11-09 11:59:17 -0800

    Completed. Minor styling update coming soon. Plus option to remove previous alarms and save data via local storage

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 $(function() {
   var alarms = [];
   var audio = new Audio('alarm1.mp3');
+  var time = moment();
+  $('.clock-container').text(time.format('h:mm:ss a')).addClass('clock');
   var myFunc = function(){
-    var time = moment();
-    $('.clock-container').text(time.format('h:mm:ss a')).addClass('clock');
     var currentTime = function(location, className){
       $(location).text(time.format('h:mm:ss a')).addClass(className);
     };
@@ -19,16 +19,16 @@ $(function() {
     myFunc();
   }, 1000);
 
-  $('button').on('click', function(){
-    var userHours = $('input.time')[0].valueAsNumber;
-    var userMinutes = $('input.time')[1].valueAsNumber;
-    var userAM = $('input.time')[2].checked;
-    var userPM = $('input.time')[3].checked;
-    var dayTime = !!userAM ? "AM" : "PM";
-    var alarmTime = userHours + ':' + userMinutes + dayTime;
-    var alarmMoment = moment({hour: userHours, minute: userMinutes})
-    alarms.push(alarmMoment);
+ $('button').on('click', function(){
+  var userHours = $('input.time')[0].valueAsNumber;
+  var userMinutes = $('input.time')[1].valueAsNumber;
+  var userAM = $('input.time')[2].checked;
+  var userPM = $('input.time')[3].checked;
+  var dayTime = !!userAM ? "AM" : "PM";
+  var alarmTime = userHours + ':' + userMinutes + dayTime; 
+  var alarmMoment = moment({hour: userHours, minute: userMinutes})
+  alarms.push(alarmMoment);
+  var li = '<li class="'+ alarmTime +'">' + alarmTime + '</li>'
     console.log(alarmTime, 'UserTime')
-    $('.user-Alarm').text(alarmTime);
-    }) 
+  }) 
 });  
